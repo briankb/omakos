1bb8ae197d8d5cae44e3a9d19cfa6c57a1cf3399
Brian Boatright <briankb@users.noreply.github.com>
2025-07-31 17:57:12 -0400

    Add option to skip Cursor IDE installation

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -7,6 +7,12 @@ source ./scripts/utils.sh
 step "Installing Homebrew packages from configs/Brewfile"
 echo ""
 echo "--------------------------------------------------------"
-brew bundle --file=configs/Brewfile
+if [ "$INSTALL_CURSOR" = "false" ]; then
+  grep -v '^cask "cursor"$' configs/Brewfile > /tmp/OmakosBrewfile
+  brew bundle --file=/tmp/OmakosBrewfile
+  rm /tmp/OmakosBrewfile
+else
+  brew bundle --file=configs/Brewfile
+fi
 echo "--------------------------------------------------------"
 print_success "Homebrew packages installed!"
